ea9f30755dc0b70f3c3488da2ce737e4a71135e5
Oswaldo Neto <oswaldo@gmail.com>
2025-03-28 15:03:03 -0300

    Remove GitHub Actions workflows

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/checklist-172/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    minify: false
-  },
+  base: '/',
   server: {
     port: 3000,
-    strictPort: true,
-  },
+    open: true
+  }
 })
